d20433f3d099fae0e256b9761bf53b6dc899ee7a
Luc Mosser <44872924+lmssr@users.noreply.github.com>
2019-09-30 17:35:24 +0200

    New component

---
+++ src/Client.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Client extends Component {
+  render() {
+    const details = this.props.details;
+    const onDelete = this.props.onDelete;
+
+    <li>
+      {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
+    </li>;
+  }
+}
+
+export default Client;
